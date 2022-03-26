48a67686f6a268b0fe262ca3a4669a3979c47414
Daniel Costa <dancosta.id@gmail.com>
2022-03-26 18:28:33 -0300

    Profile img and profile are done. img fits perfectly

--- app/javascript/components/banner.jsx
+++ app/javascript/components/banner.jsx
@@ -5,7 +5,7 @@ class Banner extends React.Component {
     return(
       <div className="banner-bg">
         <div className="avatar">
-          {/* <img src="https://res.cloudinary.com/dognkye6x/image/upload/v1648326874/IMG_0476_copy_cpnkbj.png" alt="Dan's profile photo." /> */}
+          <img className="avatar-img" src="https://res.cloudinary.com/dognkye6x/image/upload/v1648326874/IMG_0476_copy_cpnkbj.png" alt="Dan's profile photo." />
         </div>
       </div>
   );
