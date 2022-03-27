ba21cb3294783e5671886dea7921e020d37dfcb7
Daniel Costa <dancosta.id@gmail.com>
2022-03-26 23:33:47 -0300

    refactored the code for the banner and made some minor changes to the layout

--- app/javascript/components/banner.jsx
+++ app/javascript/components/banner.jsx
@@ -8,8 +8,16 @@ class Banner extends React.Component {
           <img className="avatar-img" src="https://res.cloudinary.com/dognkye6x/image/upload/v1648345738/IMG_0591_huppm8.jpg" alt="Dan's profile photo." />
         </div>
 
-        <div className="hire-btn">
-          <a class="btn btn-main" href="#">Hire Me</a>
+        <div className="buttons">
+          <div className="hire-btn">
+            <a class="btn btn-main" href="#">Hire Me</a>
+          </div>
+
+          <div className="social-icons">
+            <i class="fab fa-github"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-instagram"></i>
+          </div>
         </div>
       </div>
   );
