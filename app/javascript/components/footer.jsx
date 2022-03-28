6a01c1afd97dc33350e3734f0fab98aee63da5fb
Daniel Costa <dancosta.id@gmail.com>
2022-03-28 09:36:40 -0300

    added a footer

---
+++ app/javascript/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div class="footer">
+        <div class="footer-links">
+
+        </div>
+        <div class="footer-copyright">
+          <p>Created by Daniel Costa</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Footer
