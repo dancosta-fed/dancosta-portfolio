66f050f1ca0508803bb9fb25a0f267cdcc0ab892
Daniel Costa <dancosta.id@gmail.com>
2022-04-01 14:35:47 -0300

    I've added blog posts from medium to the blog page

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes ,Route } from 'react-router-dom';
+import * as serviceWorker from '../../helpers/serviceWorker';
 import Home from './pages/home'
 import Blog from './pages/blog'
 import Contact from './pages/contact'
