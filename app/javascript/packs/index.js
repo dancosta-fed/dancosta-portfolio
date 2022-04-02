8713160c9d89588535f91edd45af3ab1726c2f70
Daniel Costa <dancosta.id@gmail.com>
2022-04-02 11:41:48 -0300

    debugging: changed one scss file to css. Just like the tutorial said. hopeing to get the page working on ios

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Router, Routes, Route } from "react-router-dom"
 import App from '../components/app'
 import 'shards-ui/dist/css/shards.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../../assets/stylesheets/components/shards-dashboards.1.1.0.min.css'
 
 
 document.addEventListener('DOMContentLoaded', () => {
