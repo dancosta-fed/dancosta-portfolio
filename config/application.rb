f521432e43b32097e6724b58839807cb1bf7e018
Daniel Costa <dancosta.id@gmail.com>
2022-04-04 13:04:16 -0300

    fixed a error on application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,14 +11,14 @@ module DancostaPortfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    config.action_mailer.delivery_method = :smtp
-    congi.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: 587,
-      email_user_name: ENV['GMAIL_ADDRESS'],
-      password: ENV['GMAIL_APP_PASSWORD'],
-      enable_starttls_auto: true
-    }
+    # config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.smtp_settings = {
+    #   address: 'smtp.gmail.com',
+    #   port: 587,
+    #   email_user_name: ENV['GMAIL_ADDRESS'],
+    #   password: ENV['GMAIL_APP_PASSWORD'],
+    #   enable_starttls_auto: true
+    # }
 
     # Configuration for the application, engines, and railties goes here.
     #
